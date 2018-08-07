49771fabed6b5e5080d388d5b3b5bd334abb6bf1
oleg-tgn <stelmah.taganrog@gmail.com>
2018-08-07 21:19:02 +0300

    first example programs

---
+++ 1 week/FirstProject/rassengan.cpp
@@ -0,0 +1,20 @@
+/*
+* rassengan.cpp
+* Created by:Mudak
+* 7 рту. 2018 у.
+* 19:19:34
+*/
+
+#include <iostream>
+
+using namespace std;
+//Function Prototype
+
+int main()
+{
+	//Todo Place code here
+	cout << "Eclipce C++" << endl;
+	return 0;
+}
+
+//Function Definition
